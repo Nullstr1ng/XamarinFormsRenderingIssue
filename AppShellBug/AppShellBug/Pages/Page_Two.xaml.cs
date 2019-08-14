8276c76758eaee96b8efa250726d1178c1d65102
Jayson Ragasa <jayson.d.ragasa@live.com>
2019-08-15 02:25:08 +0800

    initial commit

---
+++ AppShellBug/AppShellBug/Pages/Page_Two.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppShellBug.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page_Two : ContentPage
+    {
+        public Page_Two()
+        {
+            InitializeComponent();
+        }
+    }
+}
